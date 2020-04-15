eb7225361a0633b6a748e20e6541afab35efb7b3
Ruditya <rudityap20@gmail.com>
2020-04-15 15:01:54 +0700

    Updates

---
+++ precache-manifest.9aa328017441d7072c6dc4275b705b24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90ab02fb01be46f4134357c9ef33a255",
+    "url": "/uts-web-rudityacandra-15713/index.html"
+  },
+  {
+    "revision": "2e7af3571be4894b5a39",
+    "url": "/uts-web-rudityacandra-15713/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "735d63567ab714ca8451",
+    "url": "/uts-web-rudityacandra-15713/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2e7af3571be4894b5a39",
+    "url": "/uts-web-rudityacandra-15713/static/js/2.c39e3bc8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/uts-web-rudityacandra-15713/static/js/2.c39e3bc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "735d63567ab714ca8451",
+    "url": "/uts-web-rudityacandra-15713/static/js/main.50b06698.chunk.js"
+  },
+  {
+    "revision": "b76d9d854184db044025",
+    "url": "/uts-web-rudityacandra-15713/static/js/runtime-main.bc38b26e.js"
+  }
+]);
